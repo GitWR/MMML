760a61414c94bdc07759c1eb66fb5967b61eb35e
RuiW <RunningWang@outlook.com>
2020-06-21 18:02:47 +0800

    Update compute_metric_learning.m

--- compute_metric_learning.m
+++ compute_metric_learning.m
@@ -1,7 +1,7 @@
 function [ U ] = compute_metric_learning( k_Gras, k_Spd, lamda1, lamda2, Train_lables)
-  num_class = length(unique(Train_lables)); % 类别数
+  num_class = length(unique(Train_lables)); % 
   D = size(k_Gras,1); % 141
-  d = 8; % 需要调节的参数
+  d = 8; % the target dimensionality of the generated subspace
   itera = 1;
   % U = rand(D,d);
 for i=1:itera
@@ -12,7 +12,7 @@ for i=1:itera
   Nw=0;
   Nb=0;
   for j = 1 : num_class
-      num_eachclass = find(Train_lables==j);%统计出每一类的样本的索引
+      num_eachclass = find(Train_lables==j);
       for k = 1 : length(num_eachclass)
           K_gras_data1 = k_Gras(num_eachclass(k),:);
           K_spd_data1 = k_Spd(num_eachclass(k),:);
@@ -23,14 +23,14 @@ for i=1:itera
               Sw_temp_spd = lamda2*(K_spd_data1'-K_spd_data2')*(K_spd_data1'-K_spd_data2')'*lamda2;
               Sw_temp = Sw_temp_gras + Sw_temp_spd;
               Sw = Sw+Sw_temp;
-              Nw=Nw+1;%用于统计成对的计算样本的个数
+              Nw=Nw+1;
           end
       end
   end
   
   for j=1:num_class
-      num_eachclass=find(Train_lables==j);%统计出每一类的样本的索引
-      num_difclass=find(Train_lables~=j);%统计出和j不同类的样本索引
+      num_eachclass=find(Train_lables==j);
+      num_difclass=find(Train_lables~=j);
       for k=1:length(num_eachclass)
           K_gras_data1 = k_Gras(num_eachclass(k),:);
           K_spd_data1 = k_Spd(num_eachclass(k),:);
@@ -41,29 +41,28 @@ for i=1:itera
               Sb_temp_spd = lamda2*(K_spd_data1'-K_spd_data2')*(K_spd_data1'-K_spd_data2')'*lamda2;
               Sb_temp = Sb_temp_gras + Sb_temp_spd;
               Sb=Sb+Sb_temp;
-              Nb=Nb+1;%用于统计成对的计算样本的个数
+              Nb=Nb+1;%
           end
       end
   end
   
-  Sw_final=Sw/(Nw);%论文中的公式（9）,400*400
-  Sb_final=Sb/(Nb);%论文中的公式（10）,400*400
+  Sw_final=Sw/(Nw);
+  Sb_final=Sb/(Nb);
   
-%这里的迭代终止条件怎么给，我们希望投影后类内尽量靠的近，类间尽量散得开，因此每迭代一次，就分的越开。
   %Object_function = Sw_final-alpha*Sb_final;
   % if (i>1)
   %     if (Cost(i)>=Cost(i-1))
   %         count1=count1+1;
-  %         if (count1>=1)                  （收敛性说明，因此屏蔽判定条件）
+  %         if (count1>=1)   
   %            break;
   %         end
   %     end
   % end
-  % 优化
-  [Object_V , Object_E] = eig(inv(Sw_final)*Sb_final); % 取最大值
+  % 浼樺寲
+  [Object_V , Object_E] = eig(inv(Sw_final)*Sb_final); 
   E_unsort = diag(Object_E);
-  [~ , index] = sort(E_unsort,'descend'); % 特征值降序排列
-  V_sort = Object_V(:,index);%按照特征值的顺序，将特征向量降序排列
+  [~ , index] = sort(E_unsort,'descend'); 
+  V_sort = Object_V(:,index);
   U = V_sort(:,1:d);
   
   Cost(i) = det(U'*Sb_final*U)/det(U'*Sw_final*U);
