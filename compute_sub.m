2c97c306a3bfd4c0512c93abb7cd6b0025ee240d
RuiW <RunningWang@outlook.com>
2020-06-21 18:01:07 +0800

    Update compute_sub.m

--- compute_sub.m
+++ compute_sub.m
@@ -1,18 +1,16 @@
 function [sub_t , q_value] = compute_sub(data)
-  num_t=length(data);%统计训练图像集个数
+  num_t=length(data);
   sub_t=cell(1,num_t);
 for i=1:num_t
-  sample_t=zscore(data{i});%取出每一个图像集,并中心化操作
+  sample_t=zscore(data{i}); 
   cov_t=sample_t*sample_t';
-%   cov_t=cov_t+trace(cov_t)*(1e-3)*eye(size(cov_t,1));
-  [U,~,~]=svd(cov_t);%奇异值分解
+  [U,~,~]=svd(cov_t); % SVD, used to get the orthonormal basis matrix (can form the linear subspace)
   if num_t < 40
       q_value = 12;
   else
       q_value = 40;
   end
-  sub_t{i}=U(:,1:q_value);%取出目标维数下的数据
-   %从上面计算来看，线性子空间维数q=40，而论文中的D=400，这两个参数仅针对ETH-80数据集
+  sub_t{i}=U(:,1:q_value);
 end
 end
 
